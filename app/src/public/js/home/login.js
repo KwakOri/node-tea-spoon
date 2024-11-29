98f949abb3dec6b1c5bd9d08a12bfc0ebdfafffe
kwakjaehun <jh5414092@icloud.com>
2024-11-29 14:23:39 +0900

    Feat: login form 제작
    login form 구현했음

---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
+
+loginBtn.addEventListener("click", login);
