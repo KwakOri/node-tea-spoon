fc8d1fb17dcf12ad5b907f6dcc2873c3d0bd5151
kwakjaehun <jh5414092@icloud.com>
2024-11-30 02:31:09 +0900

    Feat: mysql -> postgresql

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql");
+const { Client } = require("pg");
 
-const db = mysql.createConnection({
+const db = new Client({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 db.connect();
